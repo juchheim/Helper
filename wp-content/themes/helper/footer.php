1725ced9f35d3bbef2075234283f407c17f3b769
Ernest Juchheim <juchheim@gmail.com>
2024-08-08 18:02:35 -0500

    copyright to footer

--- wp-content/themes/helper/footer.php
+++ wp-content/themes/helper/footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'helper' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'helper' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'helper' ), 'helper', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>&copy; <?php echo date("Y"); ?> Ernest Juchheim. All rights reserved.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
